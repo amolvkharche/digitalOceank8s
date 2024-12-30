e08cf93558363b7377223d626e0c2c8fb8635e90
amolvkharche <83961171+amolvkharche@users.noreply.github.com>
2024-12-30 21:17:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "test" {
   name    = var.cluster_name
   region  = var.region
   version = var.k8s_version
-   tags = ["${var.cluster_name}"]
+  tags    = ["${var.cluster_name}"]
   node_pool {
     name       = "${var.cluster_name}-worker-pool"
     size       = var.size
