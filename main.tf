fdb155f4698a833de195b919d516fa3993c8ebba
amolvkharche <83961171+amolvkharche@users.noreply.github.com>
2024-12-30 21:17:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
-resource "digitalocean_kubernetes_cluster" "k8s" {
+resource "digitalocean_kubernetes_cluster" "test" {
   name    = var.cluster_name
   region  = var.region
   version = var.k8s_version
-  tags = ["Test-cluster"]
+   tags = ["${var.cluster_name}"]
   node_pool {
-    name       = "${var.cluster_name}-node-pool"
-    size       = "s-4vcpu-8gb"
+    name       = "${var.cluster_name}-worker-pool"
+    size       = var.size
     auto_scale = true
     min_nodes  = 2
     max_nodes  = 3
   }
 }
-
 resource "local_file" "kubeconfig" {
-  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  content  = digitalocean_kubernetes_cluster.test.kube_config.0.raw_config
   filename = "${path.module}/kubeconfig"
-  
 }
